0f17d134a9882cb0a70f10a23dba22c381cf8c03
fallnomega <fallnomega@gmail.com>
2023-02-06 07:14:58 -0800

    some base code

--- main.py
+++ main.py
@@ -1,52 +1,37 @@
-import turtle as t
-#
-# timmy = t.Turtle()
-# screen = t.Screen()
-#
-# def move_forward():
-#     timmy.forward(10)
-#
-# screen.listen()
-# screen.onkey(key="space",fun=move_forward)
-#
-# screen.exitonclick()
-#
-
-
 # EtchASketch
+import turtle as t
+
 
 def move_forward():
     sketcher.forward(50)
-def turn_left():
-    sketcher.left(90)
-def turn_right():
-    sketcher.right(90)
+
+
 def move_backwards():
+    sketcher.left(180)
     sketcher.back(50)
+
 
 def reset_screen():
     sketcher.reset()
 
+
+def turn_right():
+    new_heading = sketcher.heading() - 10
+    sketcher.setheading(new_heading)
+
+
+def turn_left():
+    new_heading = sketcher.heading() + 10
+    sketcher.setheading(new_heading)
+
+
 sketcher = t.Turtle()
 screen = t.Screen()
 screen.listen()
-screen.onkey(key='w',fun=move_forward)
-screen.onkey(key='s',fun=move_backwards)
-screen.onkey(key='d',fun=turn_right)
-screen.onkey(key='a',fun=turn_left)
-screen.onkey(key='c',fun=reset_screen)
-
-
-
-
-
-
-
-
+screen.onkey(key='w', fun=move_forward)
+screen.onkey(key='s', fun=move_backwards)
+screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='a', fun=turn_left)
+screen.onkey(key='c', fun=reset_screen)
 
 screen.exitonclick()
-
-
-
-
-
